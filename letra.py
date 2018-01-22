de65fcf566c06e3467230aaedc0a1197f5ae443c
ism47597075 <ism47597075@a06.informatica.escoladeltreball.org>
2018-01-22 13:56:41 +0100

    first commit

---
+++ letra.py
@@ -0,0 +1,8 @@
+#coding: utf -8
+letra=raw_input("indique la letra:")
+
+if letra in ("a" or "o" or "u" or "i" or "e" or "A" or "E" or "I" or "U" or "O"):
+	print "vocal"
+else:
+	print "consonante"
+
